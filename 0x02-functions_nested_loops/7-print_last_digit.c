7bb27851cfcef82e3c2da30793d8d4171e8a99a1
Samuel Adegalu <samuelhanana@gmail.com>
2022-12-16 01:49:46 -0800

    nested loop

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,9 +4,10 @@
  * print_last_digit - main function
  *
  * @n: integer to get last digit of
- * Return : last digit of n
+ * Return: last digit of n
  *
  */
+
 int print_last_digit(int n)
 {
 	_putchar('0' + _abs(n % 10));
