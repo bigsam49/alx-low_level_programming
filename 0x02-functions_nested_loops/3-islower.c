6e9d8e53d7b5741060d1d5b682a69c8b037014be
Samuel Adegalu <samuelhanana@gmail.com>
2022-12-15 23:45:09 -0800

    nested loop

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _slower - main function
+ *
+ * @c: character to compare
+ *
+ * Return: 1 if true, 0 if false
+ *
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+	
+		return(1);
+	}
+	return(0);
+}
+
